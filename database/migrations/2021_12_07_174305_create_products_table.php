023b537181a5f5fee2b4578d596f52a78d4d7a5d
vipos89 <1989146ViPoS>
2021-12-07 22:06:07 +0300

    blade views

---
+++ database/migrations/2021_12_07_174305_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255)->nullable(false);
+            $table->string('img', 255)->nullable(true);
+            $table->text('content')->nullable(true);
+            $table->integer('price')->default(0);
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
